5466fef949f5a5e7a5aba436f910a56edf448c81
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-16 12:17:40 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+// galleryContainer.addEventListener("click", onClickGallery);
+
+function createGalleryItemMarkup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+<a class = "gallery__item" href="${original}">
+  <img
+  class = "gallery__image"
+  src = "${preview}"
+  alt = "${description}"
+  data-src = "${original}">
+  </img>
+</a>
+    </div>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery__item", { captionDelay: 250 });
