0dd1ac0053f70933b1dc85d42dd996bbbed3f6ab
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-16 12:53:19 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
+var throttle = require("lodash.throttle");
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+const key = "videoplayer-current-time";
+const savedTime = localStorage.getItem(key) ? localStorage.getItem(key) : 0;
+const saveTime = seconds => localStorage.setItem(key, seconds);
+const timeUpdateHandler = e => saveTime(e.seconds);
+
+player.setCurrentTime(savedTime);
+
+player.on("timeupdate", throttle(timeUpdateHandler, 1000));
